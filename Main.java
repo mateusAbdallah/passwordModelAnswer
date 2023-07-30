e5d46712e58956def7760512e1043e084c3c98cd
Mateus Abdallah <abdallahmateus@gmail.com>
2023-07-29 22:04:12 -0700

    set a method to hold the code source

--- Main.java
+++ Main.java
@@ -3,9 +3,13 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
+		passwordModel();
+	}
+
+	static void passwordModel() {
 		Scanner input = new Scanner(System.in);
-		
+
 		String aString = "";
 		final int UPPER = 2;
 		final int LOWER = 2;
@@ -17,7 +21,7 @@ public class Main {
 		int lower = 0;
 		int digit = 0;
 		int count = 0;
-		
+
 		while(upper < UPPER || lower < LOWER || digit < DIGIT || aString.length() < 6 || detect == false) {
 			System.out.println("Enter the password");
 			aString = input.nextLine();
@@ -25,7 +29,7 @@ public class Main {
 				char ch = aString.charAt(i);
 				String cha = String.valueOf(ch);
 				count++;
-				
+
 				if(Character.isUpperCase(ch)) {
 					upper++;
 				}
@@ -38,41 +42,41 @@ public class Main {
 				else if(strBuild.toString().contains(cha)) {
 					detect = true;
 				}
-				
+
 			}
-			
-			
+
+
 			if(upper >= UPPER && lower >= LOWER && digit >= DIGIT && aString.length() >= 8 && detect == true) {
 				System.out.println("Valid password");
 			}
 			else {
 				System.out.println("The password does not have the following");
 			}
-			
+
 			if(upper < UPPER) {
 				System.out.println("* uppercase letters *");
 				System.out.println("It has " + upper + " uppercase letters, you should enter at least 2");
 			}
-			
+
 			if(lower < LOWER) {
 				System.out.println("* lower letters *");
 				System.out.println("It has " + lower + " lower letters, you should enter at least 2");
 			}
-			
+
 			if(digit < DIGIT) {
 				System.out.println("* digits *");
 				System.out.println("It has " + digit + " digits");
 			}
-			
+
 			if(aString.length() < COUNT) {
 				System.out.println("Minimum password length should be 6");
 			}
-			
+
 			if(detect == false) {
 				System.out.println("* special characters *");
 				System.out.println("It has no special characters, you should enter at least 1");
 			}
-			
+
 			upper = 0;
 			lower = 0;
 			digit = 0;
